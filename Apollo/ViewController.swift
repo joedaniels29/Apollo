7ec06182c0e8670c38e680e83e86e1b216eb1aba
Joseph Daniels <joedaniels29@me.com>
2016-10-18 09:44:43 -0400

    Initial commit

---
+++ Apollo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Apollo
+//
+//  Created by Joseph Daniels on 18/10/2016.
+//  Copyright © 2016 Joseph Daniels. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ApolloTests/ApolloTests.swift
@@ -0,0 +1,36 @@
+//
+//  ApolloTests.swift
+//  ApolloTests
+//
+//  Created by Joseph Daniels on 18/10/2016.
+//  Copyright © 2016 Joseph Daniels. All rights reserved.
+//
+
+import XCTest
+@testable import Apollo
+
+class ApolloTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
