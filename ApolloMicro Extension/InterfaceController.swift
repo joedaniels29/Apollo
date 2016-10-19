b4a15d4d7bccd131a62db2bc9e0c5390dd8e1800
Joseph Daniels <joedaniels29@me.com>
2016-10-19 10:36:45 -0400

    more targets

---
+++ ApolloMicro Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  ApolloMicro Extension
+//
+//  Created by Joseph Daniels on 19/10/2016.
+//  Copyright © 2016 Joseph Daniels. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
