f06ce8cbfbf0db90ed8e4313043357a6d57d3be8
Joseph Daniels <joedaniels29@me.com>
2016-10-19 20:51:41 -0400

    add Alamofire, gitignore

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "Apollo"
+    name: "Apollo",
+    dependencies: [
+        .Package(url: "https://github.com/ReactiveX/RxSwift.git", Version(3, 0, 0, prereleaseIdentifiers: ["rc"]))
+    ]
 )
