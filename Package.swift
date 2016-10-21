3bfa0bd4580b28646d09993901fc086345849529
Joseph Daniels <joedaniels29@me.com>
2016-10-21 10:24:50 -0400

    Add Sources

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
     name: "Apollo",
     
     dependencies: [
+        .Package(url:"../Ruby", majorVersion: 0),
         .Package(url: "https://github.com/ReactiveX/RxSwift.git", Version(3, 0, 0, prereleaseIdentifiers: ["rc"])),
          .Package(url: "https://github.com/apple/swift-protobuf.git", Version(0,9,24))
         //.Package(url: "https://github.com/Alamofire/Alamofire", Version(4, 0, 0))
