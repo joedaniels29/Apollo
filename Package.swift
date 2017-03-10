4d2d305f4b59c6f4740a8a205aa7b7c221ba2e17
Joseph Daniels <jdanie37@jh.edu>
2017-03-10 12:29:16 -0500

    Proper packaging. Objc dependency is apple platforms only

--- Package.swift
+++ Package.swift
@@ -2,9 +2,15 @@ import PackageDescription
 
 
 var targets = [
-        Target(name: "ApolloSupport", dependencies: ["Apollo"]),
-        Target(name: "Apollo")
+
 ]
+
+#if os(Linux)
+targets.append(Target(name: "Apollo"))
+#else
+targets.append(Target(name: "ApolloSupport"))
+targets.append(Target(name: "Apollo", dependencies: ["ApolloSupport"]))
+#endif
 
 #if os(macOS) || os(Linux)
 targets.append(Target(name: "ApolloServer", dependencies: ["Apollo"]))
@@ -17,8 +23,8 @@ let package = Package(
         dependencies: [
                 .Package(url: "https://github.com/ReactiveX/RxSwift.git", Version(3, 0, 0, prereleaseIdentifiers: ["rc"])),
                 .Package(url: "https://github.com/IBM-Swift/BlueSocket.git", Version(0, 12, 3)),
-                .Package(url: "https://github.com/apple/swift-protobuf.git", Version(0,9,24)),
-            	.Package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", majorVersion: 1)
+                .Package(url: "https://github.com/apple/swift-protobuf.git", Version(0, 9, 24)),
+                .Package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", majorVersion: 1)
                 //        .Package(url:"https://github.com/joedaniels29/Ruby.git", majorVersion: 0),
                 //        .Package(url: "https://github.com/Alamofire/Alamofire", Version(4, 0, 1))
         ]
