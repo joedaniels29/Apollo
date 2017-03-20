c6811a9c8bf602204f3d431c38b1ff01561b568e
Joseph Daniels <jdanie37@jh.edu>
2017-03-19 20:26:23 -0400

    add some test files, add Block service, move Structured service to its onw file

---
+++ Sources/Apollo/Services/ServiceTypes/BlockService.swift
@@ -0,0 +1,22 @@
+//
+// Created by Joseph Daniels on 3/13/17.
+// Copyright (c) 2017 Joseph Daniels. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+
+
+class BlockService: StructuredService {
+    init(name: String, block: @escaping () -> ()) {
+        super.init(name: name, running: .create { o in
+            block()
+            o.onCompleted()
+            return BooleanDisposable()
+        })
+
+    }
+
+
+}
--- Sources/Apollo/Services/ServiceTypes/StructuredService.swift
+++ Sources/Apollo/Services/ServiceTypes/StructuredService.swift
@@ -1,12 +1,10 @@
 //
-// Created by Joseph Daniels on 3/13/17.
+// Created by Joseph Daniels on 3/19/17.
 // Copyright (c) 2017 Joseph Daniels. All rights reserved.
 //
 
 import Foundation
-import RxSwift
 
-//typedef ServiceObservableBlock = (Observer)
 open class StructuredService: Service {
     //    todo: should be tied to Node Lifecycle.
     //    ie providing a starting: block should run on start()
@@ -19,7 +17,7 @@ open class StructuredService: Service {
 
     public let observable: Observable<ServiceStatusable>
     public let name: String
-      let bag:DisposeBag
+    let bag:DisposeBag
 
     public convenience init(name: String, starting: ServiceObservableBlock?, loading: ServiceObservableBlock?, running: @escaping ServiceObservableBlock, stopping: ServiceObservableBlock?, error: ServiceObservableBlock? = nil, strictLaunch: Bool = false, node:Node? = nil) {
 
@@ -28,26 +26,26 @@ open class StructuredService: Service {
     }
     public init(name: String, starting: ServiceObservable? = nil, loading: ServiceObservable? = nil, running: ServiceObservable, stopping: ServiceObservable? = nil, error: ServiceObservable? = nil, strictLaunch: Bool = false, node:Node? = nil) {
         self.name = name
-      let bag = DisposeBag()
-      self.bag = bag
-      let node : Node = node ?? StructuredService.defaultNode
+        let bag = DisposeBag()
+        self.bag = bag
+        let node : Node = node ?? StructuredService.defaultNode
 
-      if let localNode =  node as? LocalNode {
-          var observe = Observable<Observable<ServiceStatusable>>.of(
-                  ServiceObservable.just(ServiceRecord.starting).sample(localNode.starting),
-                  starting ?? .empty(),
-                  ServiceObservable.just(ServiceRecord.loading).sample(localNode.didFinishLaunching),
-                  loading ?? .empty(),
-                  ServiceObservable.just(ServiceRecord.running),
-                  running.takeUntil(localNode.willTerminate),
-                  ServiceObservable.just(ServiceRecord.stopping),
-                  stopping ?? .empty(),
-                  ServiceObservable.just(ServiceRecord.terminated)
-          ).concat()
-          if let error = error {
-              observe = observe.catchError { _ in error }
-          }
-          self.observable = observe
+        if let localNode =  node as? LocalNode {
+            var observe = Observable<Observable<ServiceStatusable>>.of(
+                    ServiceObservable.just(ServiceRecord.starting).sample(localNode.starting),
+                    starting ?? .empty(),
+                    ServiceObservable.just(ServiceRecord.loading).sample(localNode.didFinishLaunching),
+                    loading ?? .empty(),
+                    ServiceObservable.just(ServiceRecord.running),
+                    running.takeUntil(localNode.willTerminate),
+                    ServiceObservable.just(ServiceRecord.stopping),
+                    stopping ?? .empty(),
+                    ServiceObservable.just(ServiceRecord.terminated)
+            ).concat()
+            if let error = error {
+                observe = observe.catchError { _ in error }
+            }
+            self.observable = observe
 
 
         } else {
@@ -55,16 +53,4 @@ open class StructuredService: Service {
             self.observable = [starting, loading, running, stopping].flatMap { $0 }.reduce(ServiceObservable.empty()) { $0.concat($1) }
         }
     }
-}
-
-class BlockService: Service {
-    let name: String
-    init(name: String, block: @escaping () -> ()) {
-        self.name = name
-        observable = .never()
-    }
-
-    let observable: Observable<ServiceStatusable>
-
-
 }
